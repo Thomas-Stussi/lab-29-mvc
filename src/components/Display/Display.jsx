f7e349a7217724bfe0e250ed469aceecf2780406
Thomas Stussi <tstussi06@gmail.com>
2020-10-25 13:49:27 -0700

    styles

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import styles from './Display.css';
 import ReactJson from 'react-json-view';
 
 const Display = ({ display }) => {
   return (
-    <pre data-testid="display">
+    <pre data-testid="display" className={styles.Display}>
       <ReactJson src={display} displayDataTypes={false} />
     </pre>
   );
